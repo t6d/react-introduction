86a3890a0627c4df460b1c67950e7094e24075c4
Konstantin Tennhard <konstantin@tennhard.net>
2020-05-09 15:43:32 -0400

    Antd Counter Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Space } from 'antd';
+
+function useCounter() {
+  const [counter, setCount] = useState<number>(0);
+  const decrement = () => counter > 0 && setCount(counter - 1);
+  const increment = () => setCount(counter + 1)
+
+  return [counter, decrement, increment] as const;
+}
 
 function App() {
+  const style = {
+    padding: 50
+  };
+
+  const [counter, decrement, increment] = useCounter();
+
   return (
-    <div className="App">
-      Hello World!
+    <div className="App" style={style}>
+      <Space>
+        <Button type="primary" onClick={decrement}>-</Button>
+        <span>{counter}</span>
+        <Button type="primary" onClick={increment}>+</Button>
+      </Space>
     </div>
   );
 }
