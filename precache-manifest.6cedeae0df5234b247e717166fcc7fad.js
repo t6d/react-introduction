a396516e63d970e2f4e81077fe8fdfe78ec85074
Konstantin Tennhard <konstantin@tennhard.net>
2020-05-09 14:08:12 -0400

    Updates

---
+++ precache-manifest.6cedeae0df5234b247e717166fcc7fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdd2303206924ec8abe30302991a968",
+    "url": "/react-introduction/index.html"
+  },
+  {
+    "revision": "97f6c90dc89a6259c685",
+    "url": "/react-introduction/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f74734398ef56de4817",
+    "url": "/react-introduction/static/js/2.8d75e3f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-introduction/static/js/2.8d75e3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f6c90dc89a6259c685",
+    "url": "/react-introduction/static/js/main.0bcb8e93.chunk.js"
+  },
+  {
+    "revision": "54a34306b94144d3291f",
+    "url": "/react-introduction/static/js/runtime-main.9af2a429.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-introduction/static/media/logo.5d5d9eef.svg"
+  }
+]);
