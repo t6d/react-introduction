5846aa39a86c19afb5463bc3ad7a5d89e47366d1
Konstantin Tennhard <konstantin@tennhard.net>
2020-05-17 09:42:06 -0400

    Blank slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import React, { useState } from 'react';
-import { Button, Space } from 'antd';
-
-function useCounter() {
-  const [counter, setCount] = useState<number>(0);
-  const decrement = () => counter > 0 && setCount(counter - 1);
-  const increment = () => setCount(counter + 1)
-
-  return [counter, decrement, increment] as const;
-}
+import React from 'react';
 
 function App() {
-  const style = {
-    padding: 50
-  };
-
-  const [counter, decrement, increment] = useCounter();
-
   return (
-    <div className="App" style={style}>
-      <Space>
-        <Button type="primary" onClick={decrement}>-</Button>
-        <span>{counter}</span>
-        <Button type="primary" onClick={increment}>+</Button>
-      </Space>
+    <div className="App">
+      Hello World!
     </div>
   );
 }
